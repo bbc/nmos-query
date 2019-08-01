7fc710d1ba4737483261874e975c5dab66c0b799
Danny Meloy <danny.meloy@rd.bbc.co.uk>
2019-08-01 15:08:33 +0100

    Add API downgrade function from nmoscommon

--- nmosquery/version_transforms.py
+++ nmosquery/version_transforms.py
@@ -12,131 +12,32 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from nmoscommon.utils import downgrade_api_version
+
 
 def convert(obj, rtype, target_ver, downgrade_ver=None):
-    # Ensure an API version is set on the object
-    if "@_apiversion" not in obj:
-        obj["@_apiversion"] = "v1.0"
 
-    # Fix max supported API version
-    if _api_ver_compare(target_ver, "v1.3") > 0:
-        return None
-
-    # Convert high versioned resources for low versioned output
-    if _api_ver_compare(target_ver, "v1.3") < 0 and obj["@_apiversion"] == "v1.3":
-        obj = _v1_3_to_v1_2(obj, rtype)
-        obj["@_apiversion"] = "v1.2"
-    if _api_ver_compare(target_ver, "v1.2") < 0 and obj["@_apiversion"] == "v1.2":
-        obj = _v1_2_to_v1_1(obj, rtype)
-        obj["@_apiversion"] = "v1.1"
-    if _api_ver_compare(target_ver, "v1.1") < 0 and obj["@_apiversion"] == "v1.1":
-        obj = _v1_1_to_v1_0(obj, rtype)
-        obj["@_apiversion"] = "v1.0"
-
-    # Check if the object's API version is permitted in the output
-    if target_ver == obj["@_apiversion"]:
-        return obj
-    elif downgrade_ver and _api_ver_compare(obj["@_apiversion"], downgrade_ver) >= 0:
-        return obj
-
-    # Fallback
-    return None
-
-
-def _remove_if_present(obj, delete_key):
-    if isinstance(obj, list):
-        for element in obj:
-            _remove_if_present(element, delete_key)
-    if isinstance(obj, dict):
-        for key, val in obj.copy().items():
-            if isinstance(val, dict):
-                val = _remove_if_present(val, delete_key)
-            if isinstance(val, list):
-                for element in val:
-                    val = _remove_if_present(element, delete_key)
-            if key == delete_key:
-                del obj[delete_key]
-    return obj
-
-
-def _remove_keys_from_resource(obj, rtype, downgrade_mapping):
-    if rtype in downgrade_mapping:
-        for key in downgrade_mapping[rtype]:
-            _remove_if_present(obj, key)
-    return obj
-
-
-def _api_ver_compare(first, second):
-    ver_first = first[1:].split(".")
-    ver_second = second[1:].split(".")
-    if ver_first[0] < ver_second[0]:
-        return -1
-    elif ver_first[0] > ver_second[0]:
-        return 1
-    elif ver_first[1] < ver_second[1]:
-        return -1
-    elif ver_first[1] > ver_second[1]:
-        return 1
-    else:
-        return 0
-
-
-def _v1_1_to_v1_0(obj, rtype):
-
-    downgrade_mapping = {
-        'nodes': [
-            "api", "description", "tags", "clocks"
-        ],
-        "flows": [
-            "device_id", "media_type", "colorspace", "components", "frame_height", "frame_width",
-            "interlace_mode", "bit_depth", "sample_rate", "DID_SDID", "grain_rate", "transfer_characteristic"
-        ],
-        "devices": [
-            "controls", "description", "tags"
-        ],
-        "receivers": [
-            "caps"
-        ],
-        "sources": [
-            "clock_name", "channels", "grain_rate"
-        ]
+    downgrade_map = {
+        "v1.1": {
+            "node": ["description", "tags", "api", "clocks"],
+            "device": ["description", "tags", "controls"],
+            "source": ["clock_name", "grain_rate", "channels"],
+            "flow": [
+                "device_id", "grain_rate", "media_type", "sample_rate", "bit_depth", "DID_SDID", "frame_width",
+                "frame_height", "interlace_mode", "colorspace", "components", "transfer_characteristic"
+            ],
+        },
+        "v1.2": {
+            "node": ["interfaces"],
+            "sender": ["interface_bindings", "caps", "subscription"],
+            "receiver": ["interface_bindings"]
+        },
+        "v1.3": {
+            "node": ["attached_network_device", "authorization"],
+            "device": ["authorization"],
+            "source": ["event_type"],
+            "flow": ["event_type"],
+        }
     }
 
-    return _remove_keys_from_resource(obj, rtype, downgrade_mapping)
-
-
-def _v1_2_to_v1_1(obj, rtype):
-
-    downgrade_mapping = {
-        'nodes': [
-            "interfaces"
-        ],
-        "receivers": [
-            "interface_bindings"
-        ],
-        "senders": [
-            "interface_bindings", "caps", "subscription"
-        ]
-    }
-
-    return _remove_keys_from_resource(obj, rtype, downgrade_mapping)
-
-
-def _v1_3_to_v1_2(obj, rtype):
-
-    downgrade_mapping = {
-        'nodes': [
-            "attached_network_device", "authorization"
-        ],
-        "devices": [
-            "authorization"
-        ],
-        "sources": [
-            "event_type"
-        ],
-        "flows": [
-            "event_type"
-        ]
-    }
-
-    return _remove_keys_from_resource(obj, rtype, downgrade_mapping)
+    return downgrade_api_version(obj, rtype, target_ver, downgrade_map, downgrade_ver)
