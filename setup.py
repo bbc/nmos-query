687e43a461d84fbe9900eae45024652029680bb5
Danny Meloy <danny.meloy@bbc.co.uk>
2019-10-30 17:17:10 +0000

    Clean-up before stopping service thread

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ packages_required = [
 
 setup(
     name="registryquery",
-    version="0.7.8",
+    version="0.7.9",
     description="BBC implementation of an AMWA NMOS Query API",
     url='https://github.com/bbc/nmos-query',
     author='Peter Brightwell',
